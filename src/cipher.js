e0ab4b7eb5b8c2b3d1bc7a0f210aa4fb05748d72
consuelowevar <consuelowevar@MacBook-Pro-de-Mac.local>
2019-11-20 13:16:51 -0300

    codigo base

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,51 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+
+
+  /*      BOTON CIFRAR    */
+
+encode: (offsetNumber, newText) => {
+  
+  let textResult = ""; // variable donde se guardara los caracteres cifrados
+
+  for(let inicialPosition = 0 ; inicialPosition < newText.length; inicialPosition++){
+
+    let asciiText = newText.charCodeAt(inicialPosition);//charcodeAt transforma letra a nº AScii    
     
-  },
-  decode: () => {
-    /* Acá va tu código */
+    //console.log(asciiText);
+  
+    let newAscii = (asciiText - 65 + offsetNumber)% 26 + 65; //desplazamiento offset, nuevo Nº ASCII
+    let textCifrado = String.fromCharCode(newAscii); // convierte numero Ascii a letra
+    
+    textResult += textCifrado; //CONCATENA los caracteres cifrados
+    
+  } 
+  return textResult;
+
+},
+
+
+    /*    FUNCION DESCIFRAR   */
+
+decode: (offsetNumber, newText) => {
+ 
+  let textResult = ""; // variable donde se guardara los caracteres cifrados
+
+  for(let inicialPosition = 0 ; inicialPosition < newText.length; inicialPosition++){
+    
+    
+    let asciiText = newText.charCodeAt(inicialPosition);
+    
+    //console.log(asciiText);
+    
+    let newAscii = ((asciiText - 90 - offsetNumber)% 26 + 90);
+    //console.log(newAscii);
+
+    let textCifrado = String.fromCharCode(newAscii);
+
+    textResult += textCifrado;   
   }
+  return textResult;
+  
+}
+
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-/*Acá va tu código*/
+const btnacceso = document.getElementById ("acceso");
+const home = document.getElementById ("home");
+const sectionCifrado = document.getElementById ("sectionCifrado");
+
+btnacceso.addEventListener ("click",inicio);
+sectionCifrado.style.display = "none";
+
+/* FUNCION OCULTAR PANTALLA INICIO Y MOSTRAR PANTALLA CIFRADO */
+function inicio(){
+    sectionCifrado.style.display = "inline";
+    home.style.display = "none"; 
+}
+
+/*      BOTON CIFRAR    */
+const btnCifrar = document.getElementById ("actionCipher"); //
+//const textbox = document.getElementById ("textcaja"); //captura y guarda valor ingresado
+const textResult = document.getElementById ("outText");
+
+btnCifrar.addEventListener ("click", function(){
+
+    //let offset = document.getElementById("offset");
+    /*console.log(offset);*/
+    let offsetNumber = parseInt(document.getElementById("offset").value);
+    //console.log(offsetNumber);
+    let myText = document.getElementById("textcaja").value; //captura y guarda valor texto ingresado x usuario
+    let newText = myText.toUpperCase();
+    //console.log(newText);
+    //console.log(window.cipher.encode(offsetNumber, newText));
+
+    textResult.innerHTML = window.cipher.encode(offsetNumber, newText);
+});
+
+
+/*    FUNCION DESCIFRAR   */
+
+const btnDescifrar = document.getElementById("actionDescipher");
+
+btnDescifrar.addEventListener("click", function(){
+
+    //let offset = document.getElementById("offset");
+    //console.log(offset);
+    let offsetNumber = parseInt(document.getElementById("offset").value);
+    //console.log(offsetNumber);
+    let myText = document.getElementById("textcaja").value; //captura y guarda valor texto ingresado x usuario
+    let newText = myText.toUpperCase();
+    //console.log(newText);
+    //console.log(window.cipher.decode(offsetNumber, newText));
+
+    
+    textResult.innerHTML = window.cipher.decode(offsetNumber, newText);//imprime en caja resultado
+
+});
+
+
+/*      BOTON LIMPIAR   */
+ document.getElementById("limpiar").addEventListener("click", () =>{
+    document.getElementById("textcaja").value = "";// queda en blanco
+    document.getElementById("outText").value = "";//queda en blanco
+});
+
+    
